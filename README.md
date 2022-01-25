3340f25f739becef5773e97849d1e93c012b7ae6
nicolaserny <nicolas.erny@xcomponent.com>
2022-01-25 14:16:51 +0100

    Add a site example

---
+++ README.md
@@ -0,0 +1,1 @@
+# Create a React Library with TypeScript and Vite
---
+++ sites/my-site/src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import logo from './logo.svg';
+import { MyButton } from 'my-lib';
+import './App.css';
+
+function App() {
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Hello Vite + React!</p>
+                <p>count is: {count}</p>
+                <p>
+                    <MyButton onClick={() => setCount((count) => count + 1)}>Click here!</MyButton>
+                </p>
+                <p>
+                    Edit <code>App.tsx</code> and save to test HMR updates.
+                </p>
+                <p>
+                    <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                        Learn React
+                    </a>
+                    {' | '}
+                    <a
+                        className="App-link"
+                        href="https://vitejs.dev/guide/features.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Vite Docs
+                    </a>
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
